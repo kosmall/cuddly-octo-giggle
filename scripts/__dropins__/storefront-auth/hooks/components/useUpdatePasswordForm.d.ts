ded4d990a54aa50f0dfca4dac52028856e995fb9
kosmall <mac.kosmala@gmail.com>
2024-08-14 11:34:34 +0200

    Initialize fix-modal-auth

---
+++ scripts/__dropins__/storefront-auth/hooks/components/useUpdatePasswordForm.d.ts
@@ -0,0 +1,15 @@
+import { UseUpdatePasswordFormProps } from '../../types';
+
+export declare const useUpdatePasswordForm: ({ isEmailConfirmationRequired, signInOnSuccess, passwordConfigs, routeRedirectOnSignIn, routeWrongUrlRedirect, onErrorCallback, onSuccessCallback, handleSetInLineAlertProps, routeRedirectOnPasswordUpdate, }: UseUpdatePasswordFormProps) => {
+    passwordError: boolean;
+    isSuccessful: {
+        userName: string;
+        status: boolean;
+    };
+    updatePasswordValue: string;
+    isClickSubmit: boolean;
+    isLoading: boolean;
+    submitUpdatePassword: (event: Event) => Promise<void>;
+    handleSetUpdatePasswordValue: (value: string) => void;
+};
+//# sourceMappingURL=useUpdatePasswordForm.d.ts.map
